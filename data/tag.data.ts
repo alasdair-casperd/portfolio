74910b3eac5052455fe9a13a3907322aebc16c65
Alasdair Casperd <alasdaircasperd@gmail.com>
2025-02-22 17:05:52 +0000

    UI – Create tag component

--- data/tag.data.ts
+++ data/tag.data.ts
@@ -5,65 +5,76 @@ export const tags = {
     name: "TypeScript",
     tag_text: "TypeScript",
     icon: "tabler:brand-typescript",
+    hover_class: "hover:text-sky-500",
   } as Tag,
 
   swift: {
     name: "Swift",
     tag_text: "Swift",
     icon: "tabler:brand-swift",
+    hover_class: "hover:text-rose-500",
   } as Tag,
 
   nuxt: {
     name: "Nuxt",
     tag_text: "Nuxt",
     icon: "tabler:brand-nuxt",
+    hover_class: "hover:text-green-500",
   } as Tag,
 
   next: {
     name: "Next.js",
     tag_text: "next.js",
     icon: "tabler:brand-nextjs",
+    hover_class: "hover:text-white",
   } as Tag,
 
   swift_ui: {
     name: "SwiftUI",
     tag_text: "SwiftUI",
     icon: "tabler:squares",
+    hover_class: "hover:text-purple-500",
   } as Tag,
 
   vue: {
     name: "Vue",
     tag_text: "Vue",
     icon: "tabler:brand-vue",
+    hover_class: "hover:text-emerald-500",
   } as Tag,
 
   react: {
     name: "React",
     tag_text: "React",
     icon: "tabler:brand-react",
+    hover_class: "hover:text-purple-500",
   } as Tag,
 
   c_sharp: {
     name: "C#",
     tag_text: "C#",
     icon: "tabler:cube",
+    hover_class: "hover:text-lime-500",
   } as Tag,
 
   unity: {
     name: "Unity",
     tag_text: "Unity",
     icon: "tabler:brand-unity",
+    hover_class: "hover:text-cyan-500",
   } as Tag,
 
   html: {
     name: "HTML",
     tag_text: "HTML",
     icon: "tabler:brand-html5",
+    hover_class: "hover:text-pink-500",
   } as Tag,
 
   css: {
     name: "CSS",
     tag_text: "CSS",
     icon: "tabler:file-type-css",
+    hover_class: "hover:text-yellow-600",
   } as Tag,
 };
--- types/tag.ts
+++ types/tag.ts
@@ -2,4 +2,5 @@ export type Tag = {
   name: string;
   tag_text: string;
   icon: string;
+  hover_class?: string;
 };
