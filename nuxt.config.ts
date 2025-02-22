2e5bd5905773f4fa9a2d0985845402c1dcd07126
Alasdair Casperd <alasdaircasperd@gmail.com>
2025-02-22 14:22:08 +0000

    Setup – Disable nuxt component autoprefixing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,13 +3,17 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
-
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
-
+  components: [
+    {
+      path: "~/components",
+      pathPrefix: false,
+    },
+  ],
   modules: ["@nuxt/icon", "@nuxt/fonts", "@vueuse/motion/nuxt"],
 });
