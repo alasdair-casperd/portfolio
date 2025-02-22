b967e8abdbdd87fb7741e52927590c017b841325
Alasdair Casperd <alasdaircasperd@gmail.com>
2025-02-22 15:07:33 +0000

    UI – Create hero component

---
+++ components/home-page/Hero.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    class="w-full h-[550px] overflow-hidden flex flex-col justify-center items-center px-5"
+  >
+    <p
+      class="rounded-full border border-white/50 px-4 py-1 text-xs font-bold uppercase tracking-widest text-white opacity-50"
+    >
+      Hero Badge Content
+    </p>
+
+    <h1
+      class="mt-8 bg-gradient-to-br from-base-100/90 via-sky-50 to-white bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-6xl"
+    >
+      Lorem Ipsum Dolor Sit <br />Amet Consectetur Polarsi
+    </h1>
+
+    <p class="text-base-500 mt-6 text-center">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat sint iure
+      amet repellat vero quasi <br />a animi placeat fugit eos. Tenetur quo quis
+      eius, fugiat blanditiis fuga! Ipsam, vel rerum.
+    </p>
+
+    <div class="flex flex-row gap-5 mt-7">
+      <Button
+        text="Read More"
+        variant="primary"
+        icon_name="tabler:chevron-right"
+      />
+      <Button
+        text="Contact"
+        variant="secondary"
+        icon_name="tabler:chevron-right"
+      />
+    </div>
+  </div>
+</template>
