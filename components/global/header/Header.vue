ccff03fa4181f7ae02ec4f646682f911d2ed3895
Alasdair Casperd <alasdaircasperd@gmail.com>
2025-02-22 14:43:41 +0000

    UI – Create header

---
+++ components/global/header/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    class="bg-black/70 border-b border-base-900 px-4 p-2 w-full fixed z-[10000] backdrop-blur-lg text-white/80 flex flex-row justify-between items-center"
+  >
+    <div class="flex flex-row gap-3">
+      <div class="font-bold">Alasdair Casperd</div>
+      <div class="text-base-500">Portfolio</div>
+    </div>
+    <div class="flex flex-row gap-3 items-center">
+      <HeaderIcon icon_name="tabler:search" />
+      <HeaderIcon
+        icon_name="tabler:brand-github"
+        href="https://github.com/alasdair-casperd"
+      />
+      <HeaderIcon
+        icon_name="tabler:mail"
+        href="mailto:hello@alasdaircasperd.com"
+      />
+    </div>
+  </div>
+</template>
---
+++ components/global/header/HeaderIcon.vue
@@ -0,0 +1,19 @@
+<template>
+  <component
+    :is="href === undefined ? 'div' : 'a'"
+    :href="href"
+    target="_blank"
+    class="text-lg flex flex-col items-center hover:bg-base-800 p-2 rounded-lg transition-colors duration-100 cursor-pointer"
+  >
+    <Icon :name="icon_name" />
+  </component>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  icon_name: string;
+  href?: string;
+}
+
+defineProps<Props>();
+</script>
